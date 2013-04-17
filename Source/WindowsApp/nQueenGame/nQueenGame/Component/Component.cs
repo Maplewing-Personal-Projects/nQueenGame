947b5dcc52983880acae5137a3c88e1507bc31dd
Lanyi Knight <sinmaplewing@gmail.com>
2013-04-17 17:27:53 +0800

    [+][-]State is defined.

--- Source/WindowsApp/nQueenGame/nQueenGame/Component/Component.cs
+++ Source/WindowsApp/nQueenGame/nQueenGame/Component/Component.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace nQueenGame.Component
 {
-    public class Component : GameComponent
+    public class Component : GameComponent, IDrawable
     {
-        public Component(Game game) : base(game)
+        #region Property
+
+        public int DrawOrder { get; set; }
+        public bool Visible { get; set; }
+        
+        #endregion
+
+        #region Constructor
+        
+        public Component(Game game)
+            : base(game)
         {
         }
+
+        #endregion
+
+        #region Methods
+
+        public virtual void Draw(GameTime gameTime)
+        {
+        }
+        
+        #endregion
+
+        #region Event
+
+        public event EventHandler<EventArgs> DrawOrderChanged;
+        public event EventHandler<EventArgs> VisibleChanged;
+
+        #endregion
     }
 }
